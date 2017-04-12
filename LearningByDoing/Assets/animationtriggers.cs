20601e880ea35f565bb13ca7b778c5d1924a8345
Tanja-L <tanja.langer@hs-furtwangen.de>
2017-04-12 11:48:04 +0200

    animation triggers testing

---
+++ LearningByDoing/Assets/animationtriggers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationtriggers : MonoBehaviour
+{
+
+    private GameObject avatar;
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+		avatar = GameObject.Find("DefaultAvatar");
+	    animator = GetComponent<Animator>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.A))
+	    {
+	        animator.SetTrigger("forward");
+	    }
+	}
+}
